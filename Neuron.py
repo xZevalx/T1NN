bf210867a63910ba24c8b50ec6557911ab1774ba
Sebastián Pujá López <sebastian.puja.lopez@gmail.com>
2018-11-13 19:54:38 -0300

    Adds documentation to Neuron class

--- Neuron.py
+++ Neuron.py
@@ -12,6 +12,15 @@ def random_bias():
 class Neuron:
 
     def __init__(self, ninputs=1, bias=None, weights=None, learning_rate=.1):
+        """
+        Inicializa una neurona.
+
+        :param ninputs: Número de entradas que puede recibir
+        :param bias: Si no es entregado se setea aleatoriamente un número real entre -5 y 5
+        :param weights: Lista de pesos del mismo tamaño ninputs. Si no es entregado se setea aleatoriamente un número
+                        real entre -5 y 5
+        :param learning_rate: Taza de aprendizaje
+        """
         if weights is None:
             self.weights = random_weights(ninputs)
         else:
@@ -25,6 +34,12 @@ class Neuron:
         self.output = None
 
     def guess(self, x):
+        """
+        Procesa las entradas usando la función de activación
+
+        :param x: Lista de tamaño self.ninputs
+        :return: Predicción
+        """
         assert len(x) == self.ninputs
         weighted_sum = np.dot(x, self.weights) + self.bias
 
@@ -34,12 +49,27 @@ class Neuron:
         return self.output
 
     def activation_function(self, x):
+        """
+        Función de activación. Por defecto retorna la entrada
+
+        :param x: Ponderación de la entrada y los pesos de la neurona sumado a bias
+        :return: Número
+        """
         return x
 
     def update_delta(self, error):
+        """
+        Actualiza derivada de la neurona. Implementa backpropagation
+
+        """
         self.delta = error * self.output * (1.0 - self.output)
 
     def adjust_neuron_params(self):
+        """
+        Ajusta pesos y bias de acuerdo a la derivada calculada
+
+        :return:
+        """
         movement = self.lr * self.delta
         self.weights += (movement * self._last_inputs)
         self.bias += movement
